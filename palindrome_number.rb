ba1e88464bbbfcdaebd4f99f8fb0fc79074fe461
celestialheathen <jingchaoyu@hotmail.com>
2021-06-11 16:19:50 -0400

    two sum

--- palindrome_number.rb
+++ palindrome_number.rb
@@ -1,3 +1,7 @@
+# Given an integer x, return true if x is palindrome integer.
+
+# An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+
 def is_palindrome(x)
   x.to_s == x.to_s.reverse ? true : false
 end 
--- reverse_interger.rb
+++ reverse_interger.rb
@@ -1,3 +1,5 @@
+# Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
 def reverse(x) 
   if x < 0
     x.abs.to_s.reverse.to_i * -1
