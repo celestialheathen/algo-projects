83e7bfdfcb960668fe0a87cad662b9a0ed2f2e08
celestialheathen <jingchaoyu@hotmail.com>
2021-06-13 15:25:24 -0400

    valid anagram

---
+++ rotate_string.rb
@@ -0,0 +1,15 @@
+# We are given two strings, s and goal.
+# A shift on s consists of taking string s and moving the leftmost character to the rightmost position. For example, if s = 'abcde', then it will be 'bcdea' after one shift on s. Return true if and only if s can become goal after some number of shifts on s.
+
+# Example 1:
+# Input: s = 'abcde', goal = 'cdeab'
+# Output: true
+
+# Example 2:
+# Input: s = 'abcde', goal = 'abced'
+# Output: false
+
+def rotate_string(s, goal)
+  s.length.times do ||
+      
+end
